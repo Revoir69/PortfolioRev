263b7dc72ab5cd3958b276a8c77de9dacc928f74
Juan C. Sanchez <31291815+Revoir69@users.noreply.github.com>
2019-09-23 00:56:51 -0500

    Inicial

---
+++ js/mesa-manager.js
@@ -0,0 +1,48 @@
+AFRAME.registerComponent('mesa-manager', {
+         
+    init: function () {
+      let el = this.el;
+      let comp = this;
+      let data = this.data;
+      comp.scene = el.sceneEl.object3D;  
+      comp.counter = 0;   
+      comp.treeModels = [];
+      comp.modelLoaded = false;
+
+      // After gltf model has loaded, modify it materials.
+      el.addEventListener('model-loaded', function(ev){
+        let mesh = el.getObject3D('mesh'); 
+        if (!mesh){return;}
+        //console.log(mesh);
+        mesh.traverse(function(node){
+           if (node.isMesh){  
+             let mat = new THREE.MeshStandardMaterial;
+             let color = new THREE.Color(0xffffff);
+             mat.color = color;
+             mat.wireframe = true;
+             node.material = mat;
+             /*
+             if (node.material.name.includes("Trunk")){
+               // console.log(node.material);
+                  let mat = node.material;
+                  let alphaMap = new THREE.TextureLoader().load("https://cdn.glitch.com/67168196-e75b-4d1b-9851-879eb27f9d01%2FGradMask.png");
+                  mat.map = mat.map.clone();
+                  mat.transparent = true;
+                  mat.color = new THREE.Color(0xffffff);
+                  mat.alphaMap = alphaMap;   
+                  mat.alphaMap.needsUpdate = true;
+                  mat.map.needsUpdate = true;
+                  comp.treeModels.push(node);
+                 // console.log(node.name, mat.name);
+             }
+             */
+           }
+        });
+        /*console.log(comp.treeModels);
+        comp.treeModels.forEach(function(model){
+          console.log(model.name, model.material.name, model.material.map.name, model.material.map.uuid, model.material.alphaMap.name, model.material.alphaMap.uuid);
+        });*/            
+        comp.modelLoaded = true;
+      });   
+    }
+  });
---
+++ js/scene.js
@@ -0,0 +1,49 @@
+var scene = new THREE.Scene();
+controls = new THREE.TrackballControls( camera );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+var cube = new THREE.Mesh( geometry, material );
+
+scene.add( cube );
+
+
+var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+var plane = new THREE.Mesh( geometry, material );
+scene.add( plane );
+
+camera.position.z = 5;
+
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate()
+
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    controls.handleResize();
+
+    render();
+
+}
