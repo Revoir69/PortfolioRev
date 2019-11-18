5826bfe598f127f075ce69f3e13969c25bd9b782
Revoir <dezudakiddo@hotmail.com>
2019-11-17 19:40:40 -0500

    final, espero

--- js/app.js
+++ js/app.js
@@ -104,6 +104,39 @@ $(document).ready(function() {
     .to('.wrk', 1, {opacity: 1, display: 'block'})
 
 
+
+    CSSPlugin.defaultTransformPerspective = 1000;
+
+//we set the backface 
+TweenMax.set($(".cardBack"), {rotationY:-180});
+
+$.each($(".cardCont"), function(i,element) {
+  
+	var frontCard = $(this).children(".cardFront"),
+      backCard = $(this).children(".cardBack"),
+      tl = new TimelineMax({paused:true});
+	
+	tl
+		.to(frontCard, 1, {rotationY:180})
+		.to(backCard, 1, {rotationY:0},0)
+		.to(element, .5, {z:50},0)
+		.to(element, .5, {z:0},.5);
+	
+	element.animation = tl;
+  
+});
+
+$(".cardCont").hover(elOver, elOut);
+
+function elOver() {
+    this.animation.play();
+}
+
+function elOut() {
+    this.animation.reverse();
+}
+
+
 });
 
 
